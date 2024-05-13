ff675a19ecc9c7587d7a92e25a84e72a40156c9f
Usain Bald <akbjenya@gmail.com>
2024-05-13 10:28:46 +0300

    student microservice

---
+++ src/main/java/com/practice/student/services/StudentService.java
@@ -0,0 +1,26 @@
+package com.practice.student.services;
+
+import com.practice.student.entities.Student;
+import com.practice.student.repositories.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+  private final StudentRepository studentRepository;
+
+  public void saveStudent(Student student) {
+    studentRepository.save(student);
+  }
+
+  public List<Student> findAllStudents() {
+    return studentRepository.findAll();
+  }
+
+  public List<Student> findAllStudentsBySchool(Integer schoolId) {
+    return studentRepository.findAllBySchoolId(schoolId);
+  }
+}
