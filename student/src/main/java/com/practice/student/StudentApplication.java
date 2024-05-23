ab2a4e0220609d33e054e6151427c1fe5a59dbcf
Usain Bald <akbjenya@gmail.com>
2024-05-23 07:25:58 +0300

    modified student and school to work with db correctly

--- student/src/main/java/com/practice/student/StudentApplication.java
+++ student/src/main/java/com/practice/student/StudentApplication.java
@@ -2,8 +2,9 @@ package com.practice.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class StudentApplication {
 
 	public static void main(String[] args) {
--- student/src/main/java/com/practice/student/repositories/StudentRepository.java
+++ student/src/main/java/com/practice/student/repositories/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.practice.student.repositories;
 
 import com.practice.student.entities.Student;
+import lombok.Builder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
   List<Student> findAllBySchoolId(Integer schoolId);
